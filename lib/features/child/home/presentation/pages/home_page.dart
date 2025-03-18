2c21b9d79ce683f7d3722c4d5083284e509053c3
Mahmood-Anaam <eng.mahmood.anaam@gmail.com>
2025-03-18 21:38:55 +0300

    Complet Auth

--- lib/features/child/home/presentation/pages/home_page.dart
+++ lib/features/child/home/presentation/pages/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:voca_grow/features/Auth/bloc/auth_bloc.dart';
+import '../../../../../core/utils/assets.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -10,7 +11,7 @@ class HomePage extends StatelessWidget {
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(
-          title: const Text('Parent Home'),
+          title: const Text('Child Home'),
           actions: [
             IconButton(
               icon: const Icon(Icons.logout),
@@ -20,7 +21,23 @@ class HomePage extends StatelessWidget {
             ),
           ],
         ),
-        body: const Center(child: Text('Child Home')),
+        body: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage(Assets.imagesBottomPolygon),
+              alignment: Alignment.bottomRight,
+            ),
+          ),
+          child: Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(Assets.imagesTopPolygon),
+                alignment: Alignment.topLeft,
+              ),
+            ),
+            child: const Center(child: Text('Child Home')),
+          ),
+        ),
       ),
     );
   }
--- lib/features/parent/home/presentation/pages/home_page.dart
+++ lib/features/parent/home/presentation/pages/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:voca_grow/features/Auth/bloc/auth_bloc.dart';
+import '../../../../../core/utils/assets.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -20,7 +21,23 @@ class HomePage extends StatelessWidget {
             ),
           ],
         ),
-        body: const Center(child: Text('Parent Home')),
+        body: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage(Assets.imagesBottomPolygon),
+              alignment: Alignment.bottomRight,
+            ),
+          ),
+          child: Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(Assets.imagesTopPolygon),
+                alignment: Alignment.topLeft,
+              ),
+            ),
+            child: const Center(child: Text('Parent Home')),
+          ),
+        ),
       ),
     );
   }
